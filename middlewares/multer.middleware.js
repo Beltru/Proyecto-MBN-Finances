1ce73d87682dac531ce5c6ac161dccba962c39f7
Maurus <147621326+MaurusPerry@users.noreply.github.com>
2024-10-28 14:31:08 -0300

    pequeños cambios en el mlter para arreglar las cosas

--- middlewares/multer.middleware.js
+++ middlewares/multer.middleware.js
@@ -2,9 +2,18 @@ import multer from 'multer';
 
 // Configura multer para almacenar los archivos en memoria
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-console.log(upload);
+const upload = multer({ 
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // Limite de 5MB, ajusta según sea necesario
+    fileFilter: (req, file, cb) => {
+        // Solo aceptar archivos .xlsx
+        if (file.mimetype === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+            cb(null, true);
+        } else {
+            cb(new Error('Tipo de archivo no soportado. Solo se permiten archivos .xlsx.'));
+        }
+    }
+});
 
 // Middleware para manejar la subida de un solo archivo con el campo 'file'
 export const uploadFile = async (req, res, next) => {
@@ -15,12 +24,15 @@ export const uploadFile = async (req, res, next) => {
         uploadSingle(req, res, (err) => {
             if (err) {
                 // Error al subir el archivo
-                return res.status(400).json({ message: 'Error al subir el archivo: ' + err.message }); }
+                return res.status(400).json({ message: 'Error al subir el archivo: ' + err.message });
+            }
 
             // Verifica si el archivo ha sido subido
             if (!req.file) {
-                return res.status(400).json({ message: 'No se ha subido ningún archivo.' }); }
+                return res.status(400).json({ message: 'No se ha subido ningún archivo.' });
+            }
 
+            // Si todo está bien, pasa al siguiente middleware
             next();
         });
 
